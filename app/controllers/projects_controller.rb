e9921a45bf79f50cb52cafeda30383b5c0e8e598
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2012-12-10 21:44:11 -0500

    set up model, controller & views for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,13 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find params[:id]
+  end
+
+  def new
+    @project = Project.new
+  end
+end
